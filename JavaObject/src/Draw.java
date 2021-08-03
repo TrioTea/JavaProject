e400db5eb6fc15abd5709da4ecef5b1f65c1aa74
LittleStar <192354134@qq.com>
2021-08-04 01:54:08 +0800

    理解抽象

--- JavaObject/src/Draw.java
+++ JavaObject/src/Draw.java
@@ -1,12 +1,28 @@
-class Shape {
-    public void draw() {
+abstract class Shape {
+    public abstract void draw();
 
-    }
+   public void fly(){
+       System.out.println("抽象类里面的非抽象方法");
+   }
 }
 
 class Rect extends Shape {
     @Override
     public void draw() {
         System.out.println("♦");
+    }
+}
+
+class Cycle extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("○");
+    }
+}
+
+class Flower extends Shape {
+    @Override
+    public void draw() {
+        System.out.println("❀");
     }
 }
--- JavaObject/src/Test.java
+++ JavaObject/src/Test.java
@@ -1,5 +1,28 @@
 public class Test {
+    public static void drawMap(Shape shape) {
+        shape.draw();
+    }
+
     public static void main(String[] args) {
+        /*
+         Rect shape = new Rect();
+         shape.draw();
+         Cycle shape1 = new Cycle();
+         shape1.draw();
+         System.out.println("=========");
+         drawMap(shape);
+         drawMap(shape1);
+        */
+        Shape[] shapes = {new Rect(), new Cycle(), new Flower()};
+        for (Shape shape : shapes
+        ) {
+            shape.draw();
+            shape.fly();
+        }
+    }
+
+
+    public static void main1(String[] args) {
         Cat cat = new Cat();
         cat.eat();
         cat.jump();
