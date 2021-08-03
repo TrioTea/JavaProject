97cb285257c577195f8a494aafa5766799a115de
LittleStar <192354134@qq.com>
2021-08-03 22:59:51 +0800

    添加打印方法

--- JavaDemo/src/JavaDemo.java
+++ JavaDemo/src/JavaDemo.java
@@ -2,5 +2,7 @@ public class JavaDemo {
     public static void main(String[] args) {
         System.out.println("尝试新建项目推送！！");
         System.out.println("再次尝试！");
+        Test test=new Test();
+        test.print();
     }
 }
