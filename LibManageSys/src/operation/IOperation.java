c8b4161263f4a31d454b3d4599d6a1ead7642a0f
LittleStar <192354134@qq.com>
2021-08-05 03:50:40 +0800

    输入书名函数

--- LibManageSys/src/operation/IOperation.java
+++ LibManageSys/src/operation/IOperation.java
@@ -2,6 +2,13 @@ package operation;
 
 import book.BookList;
 
+import java.util.Scanner;
+
 public interface IOperation {
     void work(BookList bookList);
+    default String GetName(){
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("请输入你要操作的书名：");
+        return scanner.nextLine();
+    }
 }
