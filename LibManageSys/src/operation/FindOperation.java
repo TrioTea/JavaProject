aefccd8f9dc4c5403ced89ebd68ed638a1e331ff
LittleStar <192354134@qq.com>
2021-08-05 03:50:22 +0800

    查找图书功能实现

--- LibManageSys/src/operation/FindOperation.java
+++ LibManageSys/src/operation/FindOperation.java
@@ -1,11 +1,23 @@
 package operation;
 
+import book.Book;
 import book.BookList;
 
-public class FindOperation implements IOperation{
+public class FindOperation implements IOperation {
 
     @Override
     public void work(BookList bookList) {
         System.out.println("查找书籍：");
+        String name = this.GetName();
+
+        for (int i = 0; i < bookList.getUsedSize(); i++) {
+            Book book = bookList.getBook(i);
+            if (name.equals(book.getName())) {
+                System.out.println("找到了！！此书如下：");
+                System.out.println(book);
+                return;
+            }
+        }
+        System.out.println("没有" + name + "这本书");
     }
 }
