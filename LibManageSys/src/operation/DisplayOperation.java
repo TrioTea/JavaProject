b5fa1b6dbfa03177c523e448a51ce0e692ffc71a
LittleStar <192354134@qq.com>
2021-08-05 14:30:41 +0800

    展示图书信息更新

--- LibManageSys/src/operation/DisplayOperation.java
+++ LibManageSys/src/operation/DisplayOperation.java
@@ -2,9 +2,12 @@ package operation;
 
 import book.BookList;
 
-public class DisplayOperation implements IOperation{
+public class DisplayOperation implements IOperation {
     @Override
     public void work(BookList bookList) {
         System.out.println("展示书籍：");
+        for (int i = 0; i < bookList.getUsedSize(); i++) {
+            System.out.println(bookList.getBook(i));
+        }
     }
 }
