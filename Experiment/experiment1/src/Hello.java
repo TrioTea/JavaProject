deb069a15c8b2ce693fa08349c6e23d997edabfc
LittleStar <192354134@qq.com>
2021-09-17 00:38:46 +0800

    实验一要点1

---
+++ Experiment/experiment1/src/Hello.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("java hello!!");
+        Scanner num = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            int temp;
+            temp = Integer.parseInt(args[i]);
+            sum += temp;
+        }
+        float aver = (float) sum / 3;
+        System.out.println("平均值" + aver);
+
+    }
+}
