16a229e6773b21d3e4b23f9a9715f7ec22286562
LittleStar <192354134@qq.com>
2021-09-30 09:17:49 +0800

    图书管理

--- LibManageSys/src/Main.java
+++ LibManageSys/src/Main.java
@@ -23,6 +23,7 @@ public class Main {
     public static void main(String[] args) {
         BookList bookList = new BookList();
         User user = login();
+        //去除提示
         //noinspection InfiniteLoopStatement
         do {
             int Choice = user.menu();
