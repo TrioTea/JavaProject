9ae3472ab57c2c90cb731b9aff2c715734b36b78
LittleStar <192354134@qq.com>
2021-10-03 01:08:44 +0800

    实验四

--- Experiment/experiment4/src/person/Undergraduate.java
+++ Experiment/experiment4/src/person/Undergraduate.java
@@ -4,4 +4,5 @@ public class Undergraduate extends Student {
     public Undergraduate(int ID, String name, String sex, String birthday, int points, String field, int class_name) {
         super(ID, name, sex, birthday, points, field, class_name);
     }
+
 }
