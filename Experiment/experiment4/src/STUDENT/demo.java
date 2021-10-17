08ae41cde49448c66c760d3a9175a8678d86095e
LittleStar <192354134@qq.com>
2021-10-18 02:06:15 +0800

    实验四

--- Experiment/experiment4/src/STUDENT/demo.java
+++ Experiment/experiment4/src/STUDENT/demo.java
@@ -3,10 +3,10 @@ package STUDENT;
 public class demo {
     public static void main(String[] args) {
         Student[] students = {
-                new Postgraduate("研究生","吴浩斌", "Java", 80),
-                new Undergaduate("本科生","吴浩斌", "Java", 80),
-                new Postgraduate("研究生","吴浩斌", "python", 75),
-                new Undergaduate("本科生","吴浩斌", "python", 75)
+                new Postgraduate("研究生", "吴浩斌", "Java", 80),
+                new Undergaduate("本科生", "吴浩斌", "Java", 80),
+                new Postgraduate("研究生", "吴浩斌", "python", 75),
+                new Undergaduate("本科生", "吴浩斌", "python", 75)
         };
 
         for (Student student : students) {
