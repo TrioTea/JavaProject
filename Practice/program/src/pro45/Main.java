0350d5784d1a5c9576b77aa8aedf0c9916a938a4
LittleStar <192354134@qq.com>
2021-11-06 22:57:58 +0800

    java课后练习

--- Practice/program/src/pro45/Main.java
+++ Practice/program/src/pro45/Main.java
@@ -1,5 +1,7 @@
 package pro45;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -7,10 +9,25 @@ import java.util.Scanner;
  */
 public class Main {
     private static boolean judge(String pattern, String str) {
-        if (pattern.length() != str.split(" ").length) {
+        String[] strings = str.split(" ");
+        String[] patterns = pattern.split("");
+        if (pattern.length() != strings.length) {
             return false;
+        } else {
+            Map<String, String> map = new HashMap<>();
+            String result = "";
+            /* 建立映射 */
+            for (int i = 0; i < strings.length; i++) {
+                map.put(patterns[i], strings[i]);
+            }
+            for (int j = 0; j < patterns.length; j++) {
+                result = result.concat(map.get(patterns[j]));
+                if (j != patterns.length - 1) {
+                    result = result.concat(" ");
+                }
+            }
+            return str.equals(result);
         }
-
     }
 
     public static void main(String[] args) {
