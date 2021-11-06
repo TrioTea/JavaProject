fc79bde2bef1e05925f5d27a283cd1b7ead4f3ef
LittleStar <192354134@qq.com>
2021-11-06 22:57:19 +0800

    java课后练习

--- Practice/function/src/function3/Main.java
+++ Practice/function/src/function3/Main.java
@@ -1,9 +1,12 @@
 package function3;
 
+/**
+ * @author 比茗还明
+ */
 public class Main {
     public static void main(String[] args) {
         TicketHouse officer = new TicketHouse();
-        Thread zhangfei,likui;
+        Thread zhangfei, likui;
         zhangfei = new Thread(officer);
         zhangfei.setName("张飞");
         likui = new Thread(officer);
@@ -13,17 +16,47 @@ public class Main {
     }
 }
 
-class TicketHouse implements Runnable{
+class TicketHouse implements Runnable {
     int fiveAmount = 2, tenAmount = 0, twentyAmount = 0;
+
+    @Override
     public void run() {
-        if(Thread.currentThread().getName().equals("张飞")) {
+        if (Thread.currentThread().getName().equals("张飞")) {
             saleTicket(20);
-        }else if (Thread.currentThread().getName().equals("李逵")) {
+        } else if (Thread.currentThread().getName().equals("李逵")) {
             saleTicket(5);
         }
     }
-    /* 请在这里填写答案 */
-    private synchronized void saleTicket(int money){
 
+    /**
+     * 请在这里填写答案
+     */
+    private synchronized void saleTicket(int money) {
+        //如果使用该方法的线程传递的参数是5,就不用等待
+        if (money == 5) {
+            fiveAmount = fiveAmount + 1;
+            System.out.println("给" + Thread.currentThread().getName() + "入场券" + Thread.currentThread().getName() + "的钱正好");
+        } else if (money == 20) {
+            while (fiveAmount < 3) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "靠边等...");
+                    //如果使用该方法的线程传递的参数是20须等待
+                    wait();
+                    System.out.println(Thread.currentThread().getName() + "继续买票");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            fiveAmount = fiveAmount - 3;
+            twentyAmount = twentyAmount + 1;
+            if(Thread.currentThread().getName().equals("李逵"))
+            {
+                System.out.println("给李逵入场券," + Thread.currentThread().getName() + "给20，找赎15元");
+            }
+            else {
+                System.out.println("张飞入场券，" + Thread.currentThread().getName() + "给20，找赎15元");
+            }
+        }
+        notifyAll();
     }
 }
