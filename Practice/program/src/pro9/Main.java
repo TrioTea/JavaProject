ec5266ee3ac4130e0c820c0fc6994577c72fc8fa
LittleStar <192354134@qq.com>
2021-11-06 10:54:34 +0800

    java课后作业

---
+++ Practice/program/src/pro9/Main.java
@@ -0,0 +1,14 @@
+package pro9;
+
+import java.util.Scanner;
+
+/**
+ * @author 比茗还明
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder(scanner.nextLine());
+        System.out.println(sb.reverse());
+    }
+}
