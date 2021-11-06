61cdecb207677af455ae1ac81ef0cadf79b4ec82
LittleStar <192354134@qq.com>
2021-11-06 10:54:10 +0800

    java课后作业

---
+++ Practice/program/src/pro6_x/Main.java
@@ -0,0 +1,12 @@
+package pro6_x;
+
+/**
+ * @author 比茗还明
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[Hello]\n" +
+                "[World]\n" +
+                "[Synchronized]");
+    }
+}
