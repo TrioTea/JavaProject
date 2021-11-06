37883ab9ee70478e99f0bb52e90d493efae90f43
LittleStar <192354134@qq.com>
2021-11-06 22:57:53 +0800

    java课后练习

--- Practice/program/src/pro39/Main.java
+++ Practice/program/src/pro39/Main.java
@@ -4,9 +4,21 @@ package pro39;
  * @author 比茗还明
  */
 public class Main {
-    public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
 
+    public static void main(String[] args) {
+        int rows = 10;
+
+        for (int i = 0; i < rows; i++) {
+            int number = 1;
+            // 打印空格字符串
+            for (int j = 0; j < (rows - i - 1) * 2 + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.format("%d   ", number);
+                number = number * (i - j) / (j + 1);
+            }
+            System.out.println();
         }
     }
 }
