23ab4ed5a466d567cf91150d3c5a4f737a9009d8
LittleStar <192354134@qq.com>
2021-11-24 23:34:12 +0800

    实验8更新

---
+++ Experiment/experiment8/src/Test.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double he = in.nextFloat();
+        double s = he / 2;
+        //第一次反弹的高度
+        for (int i = 2; i <= 10; i++) {
+            he = he + s * 2;
+            //第i次落地经过多少米
+            s = s / 2;
+            //第i次反弹的高度
+        }
+        DecimalFormat df = new DecimalFormat("#.000000");
+        System.out.print("经过路程");
+        System.out.println(df.format(he));
+        System.out.println("反弹高度" + s);
+        in.close();
+    }
+}
