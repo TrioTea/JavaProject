72d138f26ed239de58c724294c26c8e76fdda95d
LittleStar <192354134@qq.com>
2021-11-24 23:34:40 +0800

    总体测试

--- Practice/program/src/Main.java
+++ Practice/program/src/Main.java
@@ -1,50 +1,21 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
-/**
- * @author 比茗还明
- */
 public class Main {
-    static class Animal {
-        public void call() {
-            System.out.println("一只动物正在叫");
-        }
-    }
-
-    static class Bird extends Animal {
-        String name;
-
-        public Bird(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public void call() {
-            super.call();
-            System.out.println(this.name + "正在鸟叫");
-        }
-    }
-
-    static class Dog extends Animal {
-        String name;
-
-        public Dog(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public void call() {
-            super.call();
-            System.out.println(this.name + "正在狗叫");
-        }
-    }
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str1 = scanner.nextLine();
-        String str2 = scanner.nextLine();
-        Bird bird = new Bird(str1);
-        Dog dog = new Dog(str2);
-        bird.call();
-        dog.call();
+        Scanner in = new Scanner(System.in);
+        double he = in.nextFloat();
+        double s = he / 2;
+        //第一次反弹的高度
+        for (int i = 2; i <= 10; i++) {
+            he = he + s * 2;
+            //第i次落地经过多少米
+            s = s / 2;
+            //第i次反弹的高度
+        }
+        System.out.print("经过路程");
+        System.out.println(he);
+        System.out.println("反弹高度" + s);
+        in.close();
     }
 }
